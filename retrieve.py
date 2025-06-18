1aae2dbbdfb959cc2578c9fa77261f5866525c0f
unknown <mhemani111@gmail.com>
2025-06-18 17:27:57 -0400

    Update retrieve.py

--- retrieve.py
+++ retrieve.py
@@ -3,4 +3,4 @@ import yfinance as yf
 def grab(stock):
     stockData = yf.Ticker(stock)
     latestPrice = stockData.history(period="1d")['Close'].iloc[-1]
-    print(f"Latest price of {stock}: ${latestPrice:.2f}")
+    return (f"Latest price of {stock}: ${latestPrice:.2f}")
